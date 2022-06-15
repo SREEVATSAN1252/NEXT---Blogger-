8b1a356a18f2bb7e4cb758c3840e132f47bf6e1f
SREEVATSAN1252 <sreevatsan1252@gmail.com>
2022-06-15 14:52:35 +0530

    2nd changes on schemas

--- sanity-medium/schemas/comment.js
+++ sanity-medium/schemas/comment.js
@@ -5,18 +5,27 @@ export default {
   fields: [
     {
       name: "name",
-
       type: "string",
     },
     {
       name: "email",
-
       type: "string",
     },
     {
       name: "comment",
-
       type: "string",
     },
+    {
+        title:"Approved",
+        name:"approved",
+        type:"boolean",
+        description:"comment wont get showed up without Approval"
+
+    },
+    {
+        name:"post",
+        type:"reference",
+        to:[{type:"post"}],
+    }
   ],
 };
